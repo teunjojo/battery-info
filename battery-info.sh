8142a29cb01d90e96f669e91d29f17cd3f220464
teunjojo <57492440+teunjojo@users.noreply.github.com>
2021-08-26 23:23:21 +0200

    Add files via upload

---
+++ battery-info.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+energyFull=$(echo | upower -i $(upower -e | grep 'BAT') | grep -oP '(?<=energy-full:         ).*[0-9]')
+energy=$(awk -vn="$(echo | upower -i $(upower -e | grep 'BAT') | grep -oP '(?<=energy:              ).*[0-9]')" 'BEGIN{print(n*100)}')
+p=$(awk -vn="$energy" -vm="$energyFull" 'BEGIN{printf("%.0f\n",n/m)}')
+c=$(upower -i $(upower -e | grep 'BAT') | grep -oP '(?<=capacity:).*[0-9]' | sed 's/\./,/' | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}')
+s=$(upower -i $(upower -e | grep 'BAT') | grep -oP '(?<=state:).*')
+v="$0 v1.0"
+usage()
+{
+cat << EOF
+
+usage: $0 [OPTION]
+
+This script shows the battery percentage.
+
+OPTIONS:
+    -p  Show battery percentage
+    -P  Show battery percentage with percentage symbol
+    -c  Show battery capacity/health
+    -C  Show battery capacity/health with percentage symbol
+    -s  Shows charging state
+    -v  Show version number of $0
+    -h  Show this message
+EOF
+}
+
+while getopts ":pPcCsvh" OPTION; do
+  case $OPTION in
+    h)
+      usage
+      exit 1
+      ;;
+    p)
+      output="$p"
+      ;;
+    P)
+      output="$p%"
+      ;;
+    c)
+      output="$c"
+      ;;
+    C)
+      output="$c%"
+      ;;
+    s)
+      output="$s"
+      ;;
+    v)
+      output="$v"
+      ;;
+    ?)
+      usage
+      exit
+      ;;
+  esac
+done
+echo $output
