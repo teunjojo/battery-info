9e33d12fb418d8a4ad57dd2247294a62d09433a7
teunjojo <mail@teunjojo.com>
2021-09-01 20:08:03 +0200

    Version 1.0.1
    No longer dependent on UPower + code optimization

--- battery-info.sh
+++ battery-info.sh
@@ -1,58 +1,50 @@
 #!/bin/bash
 
-energyFull=$(echo | upower -i $(upower -e | grep 'BAT') | grep -oP '(?<=energy-full:         ).*[0-9]')
-energy=$(awk -vn="$(echo | upower -i $(upower -e | grep 'BAT') | grep -oP '(?<=energy:              ).*[0-9]')" 'BEGIN{print(n*100)}')
-p=$(awk -vn="$energy" -vm="$energyFull" 'BEGIN{printf("%.0f\n",n/m)}')
-c=$(upower -i $(upower -e | grep 'BAT') | grep -oP '(?<=capacity:).*[0-9]' | sed 's/\./,/' | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}')
-s=$(upower -i $(upower -e | grep 'BAT') | grep -oP '(?<=state:).*')
-v="$0 v1.0"
 usage()
 {
 cat << EOF
-
 usage: $0 [OPTION]
 
 This script shows the battery percentage.
 
 OPTIONS:
-    -p  Show battery percentage
-    -P  Show battery percentage with percentage symbol
-    -c  Show battery capacity/health
-    -C  Show battery capacity/health with percentage symbol
-    -s  Shows charging state
-    -v  Show version number of $0
-    -h  Show this message
+
+    -p             Show battery percentage
+    -P             Show battery percentage with percentage symbol
+    -c             Show battery capacity/health
+    -C             Show battery capacity/health with percentage symbol
+    -s             Shows charging state
+    -v, --version  Show version number of $0
+    -h, --help     Show this message
 EOF
 }
 
-while getopts ":pPcCsvh" OPTION; do
-  case $OPTION in
-    h)
-      usage
-      exit 1
-      ;;
-    p)
-      output="$p"
-      ;;
-    P)
-      output="$p%"
-      ;;
-    c)
-      output="$c"
-      ;;
-    C)
-      output="$c%"
-      ;;
-    s)
-      output="$s"
-      ;;
-    v)
-      output="$v"
-      ;;
-    ?)
-      usage
-      exit
-      ;;
-  esac
-done
-echo $output
+dir="/sys/class/power_supply/"
+dir=$dir$(cd $dir && ls | grep 'BAT')
+[[ ! -d $dir ]] && die "Required directory not found!"
+
+charge_full=$(cat $dir/charge_full)
+charge_full_design=$(cat $dir/charge_full_design)
+charge_now=$(cat $dir/charge_now)
+percentage=$(awk -vn="$charge_now" -vm="$(awk -vn="$charge_full" 'BEGIN{print(n/100)}')" 'BEGIN{printf("%.0f\n",n/m)}')
+[[ $percentage -gt 100 ]] && percentage=100
+capacity=$(awk -vn="$charge_now" -vm="$(awk -vn="$charge_full_design" 'BEGIN{print(n/100)}')" 'BEGIN{printf("%.0f\n",n/m)}')
+status=$(cat $dir/status)
+version="$0 v1.0.1"
+
+while :; do
+    case "${1-}" in
+    -h | --help) usage
+    exit ;;
+    -v | --version) output=$version;;
+    -p) output=$percentage ;;
+    -c) output=$capacity ;;
+    -P) output=$percentage% ;;
+    -C) output=$capacity% ;;
+    -s) output=$status ;;
+    -?*) echo "Unknown option: $1">&2 ;;
+    *) break ;;
+    esac
+    shift
+  done
+[[ ! $output ]] && usage || echo $output
